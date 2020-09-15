9c5a24d90c87fc44b08173d65d636c09c33d0a14
eliot <eliot.masset@etu.univ-amu.fr>
2020-09-15 22:20:39 +0200

    Initialisation du code source

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#ifndef CRYPTOPP_ENABLE_NAMESPACE_WEAK
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#endif
+#include "cryptopp/dll.h"
+#include "cryptopp/default.h"
+#include "cryptopp/md5.h"
+
+int main(int argc, char *argv[]){
+	return 0;
+}
