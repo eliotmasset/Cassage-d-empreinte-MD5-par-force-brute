fd46c30da1e99c71318e6491ca955b1f49d9900d
MASSET Eliot <m19001463@A-F002591.salsa.univ-amu.fr>
2020-09-21 14:37:45 +0200

    Amélioration

--- aide.sh
+++ aide.sh
@@ -1,1 +1,1 @@
-g++ -Wall -o main.exe main.cpp -lcrypto++
+g++ -DNDEBUG -g3 -O2 -Wall -Wextra -o main main.cpp -l:../../amuhome/m19001463/Cassage-d-empreinte-MD5-par-force-brute/cryptopp/libcryptopp.a
--- main.cpp
+++ main.cpp
@@ -9,17 +9,31 @@
 #include "cryptopp/md5.h"
 #include "cryptopp/hex.h"
 
-int main(int argc, char *argv[]){
-	std::cout << "Allocation failed: ";
+
+std::string toMD5(std::string _msg)
+{
 	CryptoPP::HexEncoder encoder(new CryptoPP::FileSink(std::cout));
+	std::string digest;
+	CryptoPP::Weak::MD5 hash;
+	hash.Update((const CryptoPP::byte*)_msg.data(), _msg.size());
+	digest.resize(hash.DigestSize());
+	hash.Final((CryptoPP::byte*)&digest[0]);
+	
+	std::cout << "Digest: " << digest << std::endl;
+	
+	return digest;
+}
+
+int main(int argc, char *argv[]){
 	
 	if(argc==2)
 	{
-		std::cout << "Allocation failed: ";
-		using namespace CryptoPP;
-		typedef unsigned char byte;
-		HexEncoder encoder(new FileSink(std::cout));
-		std::string md5=argv[1];
+		
+		std::string msg = argv[1];
+		std::string md5msg = toMD5(msg);
+
+		std::cout << "Message: " << msg << std::endl;
+		std::cout << "Digest: " << md5msg << std::endl;
 	}
 	else
 		std::cerr << "Erreur : nombre d'arguments non valide " << std::endl;
