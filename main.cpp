18776a0dd460641cb2a2946a9a31640f75433a2e
MUNOZ Matteo <matteo.munoz@etu.univ-amu.fr>
2020-09-22 14:42:35 +0200

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstddef>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Qu'elle methode voulez vous utiliser ? (Sequentiel / MultiThread)"
+}
+
