dad8cd3d47e38d4dc712f0b727e40e53bb72e712
MUNOZ Matteo <matteo.munoz@etu.univ-amu.fr>
2020-09-22 14:45:28 +0200

    minor update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    char selection;
     std::cout << "Qu'elle methode voulez vous utiliser ? (Sequentiel / MultiThread)"
+    std::cin >> selection
+    switch (selection) {
+        case 's':
+            break;
+        case 'm':
+            break;
+    }
 }
 
