22cca1d9ce5a6fd16c058cf323cec1f6780e47fb
MUNOZ Matteo <matteo.munoz@etu.univ-amu.fr>
2020-09-29 14:34:10 +0200

    Major update on bash script

--- islibmake.bash
+++ islibmake.bash
@@ -9,7 +9,20 @@ else
 	wget -q --spider http://google.com
 
 	if [ $? -eq 0 ]; then
-    		echo "Online"
+    		read -p "Voulez vous télécharger la librairie (besoin de git) (y/n)?" CONT
+		if [ "$CONT" = "y" ]; then
+ 			
+			dpkg -s git &> /dev/null
+			if [ $? -eq 0 ]; then
+			  echo "Téléchargement et compilation de la librairie"
+    				git clone https://github.com/weidai11/cryptopp.git && cd cryptopp && make
+			else
+    				echo "Vous devez avoir le package git"
+			fi
+		else
+ 			 echo "Pour télécharger la librairie sans git référez vous à la doc";
+		fi
+
 	else
    	 echo "Offline"
 	fi
